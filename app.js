9a2a4b4e800b0f48d4a86e53912805c90c155212
Murat Can BERBER <muratcanberber@gmail.com>
2025-02-23 21:55:50 +0300

    Get All Books and Add a Book functionality added

--- app.js
+++ app.js
@@ -4,8 +4,27 @@ app.use(express.json());
 
 const PORT = 3000;
 
+// In-memory database
+let books = [];
+
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to Books API!!' });
+});
+
+// Get all books
+app.get('/api/books', (req, res) => {
+  res.json(books);
+});
+
+// Add a book
+app.post('/api/books', (req, res) => {
+  const book = {
+      id: books.length + 1,
+      title: req.body.title,
+      author: req.body.author
+  };
+  books.push(book);
+  res.status(201).json(book);
 });
 
 app.listen(PORT, () => {
